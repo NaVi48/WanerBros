0329b4bfd99f9b7f9394d2f493cafc792733ce1a
NaVi48 <163797634+NaVi48@users.noreply.github.com>
2024-05-20 05:06:37 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,38 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+ <script> const slides = document.querySelectorAll('.item');
+            const nextBtn = document.querySelector('.next');
+            const prevBtn = document.querySelector('.prev');
+            const current = document.querySelector('.active');  
+          
+            let currentIndex = 0;
+          
+            nextBtn.addEventListener('click', () => {
+              removeActiveClass();
+              currentIndex = (currentIndex + 1) % slides.length;
+              slides[currentIndex].classList.add('active');
+            });
+          
+            prevBtn.addEventListener('click', () => {
+              removeActiveClass();
+              currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+              slides[currentIndex].classList.add('active');
+            });
+          
+            function removeActiveClass() {
+              current.classList.remove('active');
+            }
+         let next = document.querySelector('.next')
+            let prev = document.querySelector('.prev')
+            
+            next.addEventListener('click', function(){
+                let items = document.querySelectorAll('.item')
+                document.querySelector('.slide').appendChild(items[0])
+            })
+            
+            prev.addEventListener('click', function(){
+                let items = document.querySelectorAll('.item')
+                document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+            })</script><section id="latest">
